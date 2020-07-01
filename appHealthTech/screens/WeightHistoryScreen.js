dab0c3a2b35e03436120f591ba3a88fec1388724
Carlos Henrique Lima Melara <charlesmelara@outlook.com>
2020-06-30 22:57:16 -0300

    Add Weight History screen

---
+++ appHealthTech/screens/WeightHistoryScreen.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import FirebaseService from '../services/FirebaseService.js';
+import {firebaseDatabase} from '../utils/firebase';
+import {Button} from '@material-ui/core';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+const styles = StyleSheet.create({
+    margin10: {margin: 10},
+    fullWidth: {flex: 1},
+    header: {alignItems: 'flex-start', justifyContent: 'flex-start', height: 60, paddingTop: 20, paddingBottom: 20, flexDirection: 'row'},
+    listItemText: {fontSize: 20, color: '#000000', marginBottom:10},
+    listItemHeader: {fontSize: 15, color: '#0000FF'},
+    item: {backgroundColor: '#c7c7c7', borderRadius: 20}
+
+});
+
+export default class MostrarHistorico extends React.Component {
+    state = {
+        dataList: null,
+    };
+
+    componentDidMount() {
+        FirebaseService.getDataList('leituras/key/historico', dataIn => this.setState({dataList: dataIn}), 10);
+    };
+
+    render() {
+        const {dataList} = this.state;
+
+        return (
+            <ScrollView style={styles.margin10}>
+    
+                <View style={styles.fullWidth}>
+                    {
+                        dataList && dataList.map(
+                            (item, index) => {
+                                return <View style={[styles.margin10, styles.item]} key={index} >
+                                    <View style={{padding:10}}>
+                                    <Text style={styles.listItemHeader}> Data </Text>
+                                    <Text style={styles.listItemText}> {item.Data} </Text>
+                                    <Text style={styles.listItemHeader}> Peso </Text>
+                                    <Text style={styles.listItemText}> {item.Peso} </Text>
+                                    </View>
+                                </View>
+                            }
+                        )
+                    }
+
+                </View>
+            </ScrollView>
+        );
+    }
+}
